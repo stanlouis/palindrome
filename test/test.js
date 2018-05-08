1ae2cb48394475b24e868cefa07611cdccad1a45
Stanley Louis <stanlouis@msn.com>
2018-05-07 21:10:08 -0400

    adding pending tests

--- test/test.js
+++ test/test.js
@@ -12,5 +12,8 @@ describe("Phrase", function () {
             let plainPalindrome = new Phrase("racecar");
             assert(plainPalindrome.palindrome());
         });
+
+        it("should return true for a mixed-case palindrome");
+        it("should return true for a palindrome with punctuation");
     });
 });
